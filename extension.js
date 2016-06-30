4726e973003c9030edcb686301bac4062e92e6b4
Johannes Seitz <johannes.w.seitz@gmail.com>
2016-06-30 22:09:35 +0200

    Refactor to ports and adapters

--- extension.js
+++ extension.js
@@ -1,9 +1,8 @@
 const St = imports.gi.St;
 const Main = imports.ui.main;
 const Tweener = imports.ui.tweener;
-const Soup = imports.gi.Soup;
 const Me = imports.misc.extensionUtils.getCurrentExtension();
-
+const getData = Me.imports.huawei._getHuaweiStatus;
 
 let text, button;
 
@@ -12,53 +11,22 @@ function _hideHello() {
     text = null;
 }
 
-function _getData() {
-  let get_cookie = Soup.Message.new('GET', 'http://www.huaweimobilewifi.com');
-  _httpSession.queue_message(get_cookie, function(session, message) {
-      let message = Soup.Message.new('GET', 'http://www.huaweimobilewifi.com/api/monitoring/status');
-      _httpSession.queue_message(message, function(session, message) {
-          if (message.status_code === 200) {
-              let content = message.response_body.data;
-              let oxml=new XML.REXML(content.replace('<?xml version="1.0" encoding="utf-8" ?>',''));
-              let batteryPercentage = oxml.rootElement.childElement("BatteryPercent").text;
-              _showString(batteryPercentage);
-          } else {
-              _showString("No HUAWEI");
-          }
-    });
-  });
-}
 
 function _showString(string){
-       if (!text) {
-           text = new St.Label({ style_class: 'helloworld-label', text: string });
-           Main.uiGroup.add_actor(text);
-       }
+    if (!text) {
+        text = new St.Label({ style_class: 'helloworld-label', text: string });
+        Main.uiGroup.add_actor(text);
+    }
        
-       text.opacity = 255;
-       
-       /*
-         we have to choose the monitor we want to display the hello world label. Since in gnome-shell
-         always has a primary monitor, we use it(the main monitor)
-        */
-       let monitor = Main.layoutManager.primaryMonitor;
-   
-       /*
-        we change the position of the text to the center of the monitor.
-        */
-       text.set_position(Math.floor(monitor.width / 2 - text.width / 2),
+    text.opacity = 255;
+    let monitor = Main.layoutManager.primaryMonitor;
+    text.set_position(Math.floor(monitor.width / 2 - text.width / 2),
                          Math.floor(monitor.height / 2 - text.height / 2));
-   
-       /*And using tweener for the animations, we indicate to tweener that we want
-         to go to opacity 0%, in 2 seconds, with the type of transition easeOutQuad, and,
-         when this animation has completed, we execute our function _hideHello.
-         REFERENCE: http://hosted.zeh.com.br/tweener/docs/en-us/
-        */
-       Tweener.addTween(text,
-                        { opacity: 0,
-                          time: 2,
-                          transition: 'easeOutQuad',
-                          onComplete: _hideHello });
+    Tweener.addTween(text,
+                    { opacity: 0,
+                      time: 2,
+                      transition: 'easeOutQuad',
+                      onComplete: _hideHello });
 }
 
 function init() {
@@ -71,16 +39,25 @@ function init() {
     let icon = new St.Icon({ icon_name: 'system-run-symbolic',
                              style_class: 'system-status-icon' });
     button.set_child(icon);
-    button.connect('button-press-event', _getData);
+    button.connect('button-press-event', _showData);
+}
+
+function _showData() {
+    getData().then(function(response) {
+        let battery = response["batteryPercentage"];
+        _showString(battery);
+    }, function(error) {
+        _showString("ERROR");
+    });
 }
 
 function enable() {
     Main.panel._rightBox.insert_child_at_index(button, 0);
-        //this._eventLoop = Mainloop.timeout_add_seconds(settings.get_int('update-time'), Lang.bind(this, function (){
-       //     this._querySensors();
-            // readd to update queue
-       //     return true;
-        //}));
+    //this._eventLoop = Mainloop.timeout_add_seconds(settings.get_int('update-time'), Lang.bind(this, function (){
+    //     this._querySensors();
+    // readd to update queue
+    //     return true;
+    //}));
 }
 
 function disable() {
---
+++ huawei.js
@@ -0,0 +1,46 @@
+const Me = imports.misc.extensionUtils.getCurrentExtension();
+const XML = Me.imports.rexml;
+const Soup = imports.gi.Soup;
+const Promise = Me.imports.promise.Promise;
+
+// TODO: timeout?
+const _httpSession = new Soup.SessionAsync();
+Soup.Session.prototype.add_feature.call(_httpSession, new Soup.ProxyResolverDefault());
+Soup.Session.prototype.add_feature.call(_httpSession, new Soup.CookieJar());
+
+function _getHuaweiStatus() {
+    return _firstRequestToSetSessionCookie().then(function() {
+        return _secondRequestToGetMonitoringStatus();
+    });
+}
+
+function _firstRequestToSetSessionCookie(done) {
+    return new Promise(function(resolve, reject) {
+        let get_cookie = Soup.Message.new('GET', 'http://www.huaweimobilewifi.com');
+        _httpSession.queue_message(get_cookie, function(session, message){
+            resolve(message);
+        });
+    });
+}
+
+function _secondRequestToGetMonitoringStatus(){
+    return new Promise(function(resolve, reject) {
+        let message = Soup.Message.new('GET', 'http://www.huaweimobilewifi.com/api/monitoring/status');
+        _httpSession.queue_message(message, function(session, message) {
+            if (message.status_code === 200) {
+                let content = message.response_body.data;
+                resolve(_extractDataFromStatusXML(content));
+            } else {
+                reject(Error("Huawei not reachable"));
+            }
+        });
+    });
+}
+
+function _extractDataFromStatusXML(theStatusXML) {
+    let oxml=new XML.REXML(theStatusXML.replace('<?xml version="1.0" encoding="utf-8" ?>',''));
+    let batteryPercentage = oxml.rootElement.childElement("BatteryPercent").text;
+    return {
+        "batteryPercentage" : batteryPercentage
+    };
+}
