fc503098f1706e6836176c69b261daf7fa64dbfb
Johannes Seitz <johannes.w.seitz@gmail.com>
2016-07-05 23:44:09 +0200

    Start building up menu for more information

--- extension.js
+++ extension.js
@@ -10,7 +10,7 @@ const getData = Me.imports.huawei._getHuaweiStatus;
 const Mainloop = imports.mainloop;
 const Lang = imports.lang;
 
-let _indicator, _backgroundUpdateTag;
+let _indicator, _backgroundUpdateTag, _batteryEntry, _messageEntry, _usersEntry;
 
 function init(extensionMeta) {
     let theme = imports.gi.Gtk.IconTheme.get_default();
@@ -20,6 +20,7 @@ function init(extensionMeta) {
 function enable() {
     let pollIntervalInSeconds = 2;
     _indicator = new HuaweiStatusMenu();
+    _updateMenubar();
     Main.panel.addToStatusArea('huawei-status-menu', _indicator, 0, 'right');
     _backgroundUpdateTag = Mainloop.timeout_add_seconds(pollIntervalInSeconds, Lang.bind(this, function (){
          _updateMenubar();
@@ -31,12 +32,9 @@ function _updateMenubar() {
     getData().then(function(response) {
         let signalStrength = response["signalIcon"];
         _indicator.changeIcon('signal-' + signalStrength);
+	_indicator.changeNetworkLabel(response["networkType"]);
         _indicator.actor.show();
     }, function(error) {
-        global.log("ERROR");
-        for (func in _indicator) {
-            global.log(func);
-        }
         _indicator.actor.hide();
     });
 }
@@ -52,17 +50,69 @@ const HuaweiStatusMenu = new Lang.Class({
 
     _init: function() {
         this.parent(0.0, "Huawei Status");
+        let hbox = new St.BoxLayout({ style_class: 'panel-status-menu-box' });
         this._icon = new St.Icon({ icon_name: 'signal-0',
                              style_class: 'system-status-icon huawei-status-icon' });
-	this.actor.add_child(this._icon);
+	hbox.add_child(this._icon);
+        this._connectionLabel = new St.Label({ text: _("3G"),
+                                   y_expand: true,
+                                   y_align: Clutter.ActorAlign.CENTER });
+	hbox.add_child(this._connectionLabel);
+        this.actor.add_actor(hbox);
+	_batteryEntry = new HuaweiMenuItem({icon : "battery", label : _("NO DATA")});
+	this.menu.addMenuItem(_batteryEntry);
+	_usersEntry = new HuaweiMenuItem({icon : "network-wireless", label : _("NO DATA")});
+	this.menu.addMenuItem(_usersEntry);
+	_messageEntry = new HuaweiMenuItem({icon : "mail-unread", label : _("NO DATA")});
+	this.menu.addMenuItem(_messageEntry);
+    },
+
+    changeIcon: function(newIcon) {
+        this._icon.set_icon_name(newIcon);
+    },
+
+    changeNetworkLabel: function(newNetworkLabel) {
+        this._connectionLabel.set_text(_(newNetworkLabel));
     },
 
     destroy: function() {
         this.parent();
     },
 
-    changeIcon: function(newIcon) {
-        this._icon.set_icon_name(newIcon);
+});
+
+
+const HuaweiMenuItem = new Lang.Class({
+    Name: 'PlaceMenuItem',
+    Extends: PopupMenu.PopupBaseMenuItem,
+
+    _init: function(info) {
+	this.parent();
+	this._info = info;
+        this._icon = new St.Icon({ icon_name: info.icon,
+                                   style_class: "system-status-icon huawei-popup-menu-item",
+                                   icon_size: 16 });
+	this.actor.add_child(this._icon);
+
+        this._label = new St.Label({ text: info.label });
+        this.actor.add_child(this._label);
+    },
+
+    setIcon: function(newIconName) {
+	this._icon.set_icon_name(newIconName);
+    },
+
+    setLabel: function(newLabelText) {
+	this._label.set_text(_(newLabelText));
+    },
+
+    activate: function(event) {
+	this.parent(event);
+    },
+
+    destroy: function() {
+        this.parent();
     },
 });
+
 
--- huawei.js
+++ huawei.js
@@ -40,9 +40,64 @@ function _secondRequestToGetMonitoringStatus(){
 function _extractDataFromStatusXML(theStatusXML) {
     let oxml=new XML.REXML(theStatusXML.replace('<?xml version="1.0" encoding="utf-8" ?>',''));
     let batteryPercentage = oxml.rootElement.childElement("BatteryPercent").text;
+    let wifiUsers = oxml.rootElement.childElement("CurrentWifiUser").text;
+    let networkType = oxml.rootElement.childElement("CurrentNetworkType").text;
     let signalIcon = oxml.rootElement.childElement("SignalIcon").text;
+
+	global.log(networkType);
     return {
-        "batteryPercentage" : batteryPercentage,
+	"networkType": _getNetworkType(networkType),
+        "wifiUsers": wifiUsers,
         "signalIcon" : signalIcon
     };
+}
+
+function _getNetworkType(networkType) {
+    var NO_SERVICE = '0';
+    var GSM = '1';
+    var GPRS = '2';
+    var EDGE = '3';
+    var WCDMA = '4';
+    var HSDPA = '5';
+    var HSUPA = '6';
+    var HSPA = '7';
+    var TDSCDMA = '8';
+    var HSPA_PLUS = '9';
+    var EVDO_REV_0 = '10';
+    var EVDO_REV_A = '11';
+    var EVDO_REV_B = '12';
+    var RTT = '13';
+    var UMB = '14';
+    var EVDV = '15';
+    var RTT = '16';
+    var HSPA_PLUS_64QAM = '17';
+    var HSPA_PLUS_MIMO = '18';
+    var LTE = '19';
+ 
+    switch (networkType){
+	case NO_SERVICE:
+	    return "";
+	case GSM:
+	case GPRS:
+	case EDGE:
+	case RTT:
+	case EVDV:
+	    return "2G";
+	case WCDMA:
+	case TDSCDMA:
+	case EVDO_REV_0:
+	case EVDO_REV_A:
+	case EVDO_REV_B:
+	case HSDPA:
+	case HSUPA:
+	case HSPA:
+	case HSPA_PLUS:
+	case HSPA_PLUS_64QAM:
+	case HSPA_PLUS_MIMO:
+	    return "3G";
+	case LTE:
+	    return "4G";
+	default:
+	    return "??";
+   }
 }
